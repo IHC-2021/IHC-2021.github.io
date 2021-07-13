a5262f8224e3bc04924dd1ff8d84e91f7a56188d
viniciusbaca <viniciusfbaca@usp.br>
2021-07-12 21:07:46 -0300

    feat: clean StartPage

--- lib/Screens/Start/mainStart.dart
+++ lib/Screens/Start/mainStart.dart
@@ -1,38 +1,44 @@
-import 'package:final_project_uiux/Screens/Start/carousel.dart';
+import 'package:final_project_uiux/Screens/HomePage/mainHomePage.dart';
+import 'package:final_project_uiux/Screens/ShoppingCart/mainShoppingCart.dart';
 import 'package:final_project_uiux/Screens/Start/footer.dart';
 import 'package:final_project_uiux/Screens/Start/header.dart';
 import 'package:final_project_uiux/Screens/Start/nave.dart';
-import 'package:final_project_uiux/Screens/Start/products.dart';
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatefulWidget {
   const StartScreen({Key? key}) : super(key: key);
+
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  var selectedScreen = 'inicio';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Header(),
-            Nave(),
-            Carousel(),
-            Products(),
-            Footer(),
-          ],
+        child: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.all(40),
+              constraints: BoxConstraints(
+                minWidth: 800,
+                maxWidth: 1100,
+              ),
+              child: Column(
+                children: [
+                  Header(),
+                  Nave(),
+                  selectedScreen == 'inicio' ? HomePage() : ShoppingCart(),
+                  Footer(),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
