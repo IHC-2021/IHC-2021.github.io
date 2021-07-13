4403ef3f987593e907f3f194ecd47b8406cccb88
viniciusbaca <viniciusfbaca@usp.br>
2021-07-12 21:07:06 -0300

    feat: new directory ShoppingCart

---
+++ lib/Screens/ShoppingCart/mainShoppingCart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCart extends StatefulWidget {
+  const ShoppingCart({Key? key}) : super(key: key);
+
+  @override
+  _ShoppingCartState createState() => _ShoppingCartState();
+}
+
+class _ShoppingCartState extends State<ShoppingCart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
