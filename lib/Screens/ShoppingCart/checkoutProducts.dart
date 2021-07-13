974355cca6db2fb1083e4abb9023a957713298fd
viniciusbaca <viniciusfbaca@usp.br>
2021-07-12 21:33:05 -0300

    feat: shoppingCart screen

---
+++ lib/Screens/ShoppingCart/checkoutProducts.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class CheckoutProducts extends StatefulWidget {
+  const CheckoutProducts({Key? key}) : super(key: key);
+
+  @override
+  _CheckoutProductsState createState() => _CheckoutProductsState();
+}
+
+class _CheckoutProductsState extends State<CheckoutProducts> {
+
+  Widget bananaExample(){
+    return Container(
+      color: Colors.grey,
+      margin: EdgeInsets.all(5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(
+              height: 80,
+              image: AssetImage('assets/banana.jpg')),
+          Column(
+            children: [
+              Text('abanana'),
+              Text('R\$ 23,32'),
+
+            ],
+          ),
+          Text('-'),
+          Text('1'),
+          Text('+'),
+          Text('Unidade(s)'),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          bananaExample(),
+          bananaExample(),
+          bananaExample(),
+        ],
+      ),
+    );
+  }
+}
--- lib/Screens/ShoppingCart/mainShoppingCart.dart
+++ lib/Screens/ShoppingCart/mainShoppingCart.dart
@@ -1,3 +1,6 @@
+import 'package:final_project_uiux/Screens/ShoppingCart/banner.dart';
+import 'package:final_project_uiux/Screens/ShoppingCart/checkoutButtons.dart';
+import 'package:final_project_uiux/Screens/ShoppingCart/checkoutProducts.dart';
 import 'package:flutter/material.dart';
 
 class ShoppingCart extends StatefulWidget {
@@ -10,6 +13,16 @@ class ShoppingCart extends StatefulWidget {
 class _ShoppingCartState extends State<ShoppingCart> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.all(10),
+      color: Colors.purpleAccent,
+      child: Column(
+        children: [
+          CheckoutBanner(),
+          CheckoutProducts(),
+          CheckoutButtons(),
+        ],
+      ),
+    );
   }
 }
